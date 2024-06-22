6b748b3f349404822bc101ba7ce17cac1cc2b729
JoG655 <gregov.josip@gmail.com>
2024-06-22 11:19:19 +0200

    Raised Z-index on page transition

--- src/components/TransitionSlide.tsx
+++ src/components/TransitionSlide.tsx
@@ -10,14 +10,14 @@ export function TransitionSlide({ children }: TransitionSlideProps) {
     <>
       {children}
       <motion.div
-        className="fixed left-0 top-0 h-dvh w-dvw origin-bottom bg-primary-700 dark:bg-primary-400"
+        className="fixed left-0 top-0 z-50 h-dvh w-dvw origin-bottom bg-primary-700 dark:bg-primary-400"
         initial={{ scaleY: 0 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 1 }}
         transition={{ duration: 0.3, ease: "linear" }}
       />
       <motion.div
-        className="fixed left-0 top-0 h-dvh w-dvw origin-top bg-primary-700 dark:bg-primary-400"
+        className="fixed left-0 top-0 z-50 h-dvh w-dvw origin-top bg-primary-700 dark:bg-primary-400"
         initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 0 }}
